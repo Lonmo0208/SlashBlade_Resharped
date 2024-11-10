a96afe1873f7345ee9301c3fe3dab089be126c1f
Lonmo0208 <95102577+Lonmo0208@users.noreply.github.com>
2024-11-10 17:09:48 +0800

    Update EntityAbstractSummonedSword.java

--- src/main/java/mods/flammpfeil/slashblade/entity/EntityAbstractSummonedSword.java
+++ src/main/java/mods/flammpfeil/slashblade/entity/EntityAbstractSummonedSword.java
@@ -595,7 +595,7 @@ public class EntityAbstractSummonedSword extends Projectile implements IShootabl
         return ProjectileUtil.getEntityHitResult(this.level(), this, p_213866_1_, p_213866_2_,
                 this.getBoundingBox().expandTowards(this.getDeltaMovement()).inflate(1.0D), (entity) -> {
                     return entity.canBeHitByProjectile()
-                            && (entity != this.getShooter() || this.ticksInAir >= 5)
+                            && (entity != this.getShooter() || this.ticksInAir >= 10)
                             && (this.alreadyHits == null || !this.alreadyHits.contains(entity.getId()));
                 });
     }
